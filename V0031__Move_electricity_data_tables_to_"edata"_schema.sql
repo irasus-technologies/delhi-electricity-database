410ff4cad5a384930e6d49afa083a6f597bf94ea
Anirudh Ramesh <ramesh.anirudh95@gmail.com>
2019-11-11 00:15:16 +0530

    Move electricity data tables to "edata" schema

---
+++ V0031__Move_electricity_data_tables_to_"edata"_schema.sql
@@ -0,0 +1,23 @@
+ALTER TABLE public.discoms
+    SET SCHEMA edata;
+
+ALTER TABLE public.energy_export
+    SET SCHEMA edata;
+
+ALTER TABLE public.energy_import
+    SET SCHEMA edata;
+
+ALTER TABLE public.plants_centre
+    SET SCHEMA edata;
+
+ALTER TABLE public.plants_state
+    SET SCHEMA edata;
+
+ALTER TABLE public.states
+    SET SCHEMA edata;
+
+ALTER TABLE public.substations
+    SET SCHEMA edata;
+
+ALTER TABLE public.frequency
+    SET SCHEMA edata;
