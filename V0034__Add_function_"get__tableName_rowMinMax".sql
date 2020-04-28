020827de124c0338f60fce70f598c91eb02b599d
Anirudh Ramesh <ramesh.anirudh95@gmail.com>
2020-04-28 18:21:48 +0530

    Add function "get__tableName_rowMinMax"

---
+++ V0034__Add_function_"get__tableName_rowMinMax".sql
@@ -0,0 +1,30 @@
+CREATE OR REPLACE FUNCTION public."get__tableName_rowMinMax()
+RETURNS text[]
+LANGUAGE PLPGSQL
+AS $$
+DECLARE
+    schematablename TEXT[];
+    tablename TEXT;
+    rowcounts TEXT[];
+    rowcount TEXT;
+    rowmin TEXT;
+    rowmax TEXT;
+    i INTEGER := 1;
+BEGIN
+    FOR tablename IN
+        SELECT
+            CONCAT(table_schema, '.', table_name)
+        FROM
+            information_schema.tables
+        WHERE
+            table_schema IN ('edata') AND
+            table_type = 'BASE TABLE'
+    LOOP
+        schematablename := STRING_TO_ARRAY(tablename, '.');
+        EXECUTE FORMAT('SELECT COUNT(*), MIN("serialNumber"), MAX("serialNumber") FROM %I.%I', schematablename[1], schematablename[2]) INTO rowcount, rowmin, rowmax;
+        rowcounts[i] := FORMAT('%I,%I,%I,%I', tablename, rowcount, rowmin, rowmax);
+        i := i + 1;
+    END LOOP;
+    RETURN rowcounts;
+END
+$$;
